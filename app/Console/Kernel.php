78e254708368b2841da4800a29175d6a020efa6e
Peter Blay <52701136+Bostigger@users.noreply.github.com>
2023-12-24 19:25:23 +0000

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('emails:send-to-subscribers')->hourly();
+        $schedule->command('app:send-emails-to-subscribers')->hourly();
     }
 
 
